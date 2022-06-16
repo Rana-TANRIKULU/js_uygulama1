adbc2451eaf70e2b12564bc496c4e86c2f898923
Rana-TANRIKULU <ranatanrikuluu@gmail.com>
2022-06-16 12:22:26 +0300

    ilk uygulamalarım

---
+++ script.js
@@ -0,0 +1,65 @@
+// ** Bir müşterinin aşağıdaki bilgileri için değişken oluşturunuz.
+
+// ** Müşteri adı 
+// ** Müşteri soyadı
+// ** Müşteri tc  kimlik
+// ** Müşteri sipariş toplamı
+// ** Müşteri cinsiyet
+// ** Müşteri adres bilgisi
+// ** Müşteri hobileri
+
+var customerName='Ceyda';
+var customerLastName='Tanrıkulu';
+
+var fullName='Ceyda Tanrıkulu';
+var musteriId='1322323232';
+
+var total = 205.6;
+var gender =true;//erkek false,kadın true
+
+//object
+var address={
+    city:'Aydın',
+    district:'İncirliova',
+    body:'Ömerağa mah.No:45'
+}
+
+//array
+var hobbies=['Sinema','Kitap','Spor'];
+
+
+// ** Aşağıdaki siparişlerin toplamını hesaplayınız.
+
+var order1=Number('100');
+var order2=Number('150');
+
+var totalOrder= order1 + order2;
+console.log(totalOrder);
+
+// ** Aşağıdaki siparişlerin toplamını hesaplayınız.
+
+var order3=Number('100.2');
+var order4=Number('150.5');
+
+var totalOrder2= order3 + order4;
+console.log(totalOrder2);
+
+// ** Aşağıdaki siparişlerin toplamını tam sayı olarak hesaplayınız.
+
+var order4=parseInt('100.2');
+var order5=parseInt('150.5');
+
+var totalOrder3= order4 + order5
+console.log (totalOrder3)
+
+// ** Aşağıdaki verilen doğum yılına göre yaş hesaplayınız.
+
+const yearOfBirth = 2014;
+
+console.log(new Date().getFullYear()-yearOfBirth)
+
+// ** Aşağıdaki string ifadenin karakter sayısını bulunuz.
+
+let course ='Modern Javascript Kursu';
+
+console.log(course.length)
