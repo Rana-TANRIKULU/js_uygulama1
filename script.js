81efd4325809be89e402e006490c003eb699021a
Rana-TANRIKULU <ranatanrikuluu@gmail.com>
2022-06-18 15:10:44 +0300

    uygulamalarım

--- script.js
+++ script.js
@@ -8,58 +8,93 @@
 // ** Müşteri adres bilgisi
 // ** Müşteri hobileri
 
-var customerName='Ceyda';
-var customerLastName='Tanrıkulu';
+// var customerName='Ceyda';
+// var customerLastName='Tanrıkulu';
 
-var fullName='Ceyda Tanrıkulu';
-var musteriId='1322323232';
+// var fullName='Ceyda Tanrıkulu';
+// var musteriId='1322323232';
 
-var total = 205.6;
-var gender =true;//erkek false,kadın true
+// var total = 205.6;
+// var gender =true;//erkek false,kadın true
 
-//object
-var address={
-    city:'Aydın',
-    district:'İncirliova',
-    body:'Ömerağa mah.No:45'
-}
+// //object
+// var address={
+//     city:'Aydın',
+//     district:'İncirliova',
+//     body:'Ömerağa mah.No:45'
+// }
 
-//array
-var hobbies=['Sinema','Kitap','Spor'];
+// //array
+// var hobbies=['Sinema','Kitap','Spor'];
 
 
-// ** Aşağıdaki siparişlerin toplamını hesaplayınız.
+// // ** Aşağıdaki siparişlerin toplamını hesaplayınız.
 
-var order1=Number('100');
-var order2=Number('150');
+// var order1=Number('100');
+// var order2=Number('150');
 
-var totalOrder= order1 + order2;
-console.log(totalOrder);
+// var totalOrder= order1 + order2;
+// console.log(totalOrder);
 
-// ** Aşağıdaki siparişlerin toplamını hesaplayınız.
+// // ** Aşağıdaki siparişlerin toplamını hesaplayınız.
 
-var order3=Number('100.2');
-var order4=Number('150.5');
+// var order3=Number('100.2');
+// var order4=Number('150.5');
 
-var totalOrder2= order3 + order4;
-console.log(totalOrder2);
+// var totalOrder2= order3 + order4;
+// console.log(totalOrder2);
 
-// ** Aşağıdaki siparişlerin toplamını tam sayı olarak hesaplayınız.
+// // ** Aşağıdaki siparişlerin toplamını tam sayı olarak hesaplayınız.
 
-var order4=parseInt('100.2');
-var order5=parseInt('150.5');
+// var order4=parseInt('100.2');
+// var order5=parseInt('150.5');
 
-var totalOrder3= order4 + order5
-console.log (totalOrder3)
+// var totalOrder3= order4 + order5
+// console.log (totalOrder3)
 
-// ** Aşağıdaki verilen doğum yılına göre yaş hesaplayınız.
+// // ** Aşağıdaki verilen doğum yılına göre yaş hesaplayınız.
 
-const yearOfBirth = 2014;
+// const yearOfBirth = 2014;
 
-console.log(new Date().getFullYear()-yearOfBirth)
+// console.log(new Date().getFullYear()-yearOfBirth)
 
-// ** Aşağıdaki string ifadenin karakter sayısını bulunuz.
+// // ** Aşağıdaki string ifadenin karakter sayısını bulunuz.
 
-let course ='Modern Javascript Kursu';
+// let course ='Modern Javascript Kursu';
 
-console.log(course.length)
+// console.log(course.length)
+
+
+
+// Oparetörler
+
+
+
+let val;
+const a=10;
+const b=5;
+let c=3;
+
+// 1- Aritmetik Oparetörler
+
+val = a+b;
+val = a-b;
+val = a*b;
+val = a/b;
+val = a%b;
+val = c++
+val = ++c
+val = --c
+val = c--
+    
+// 2- Atama Oparetörleri
+
+val = a;
+val +=a;
+
+// 3- Karşılaştırma Oparetörleri
+
+// 4-Mantıksal Oparetörler
+
+console.log(val);
+console.log(typeof val);
