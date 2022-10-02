74885d8b502815e7ae1d539dd763fbd60e227dbe
Rana-TANRIKULU <ranatanrikuluu@gmail.com>
2022-10-02 20:18:39 +0300

    uygulam1

--- script.js
+++ script.js
@@ -70,65 +70,65 @@
 
 
 
-let val;
-const a=10;
-const b=5;
-const  c=5;
-let d=3;
+// let val;
+// const a=10;
+// const b=5;
+// const  c=5;
+// let d=3;
 
-// 1- Aritmetik Oparetörler
+// // 1- Aritmetik Oparetörler
 
-val = a+b;
-val = a-b;
-val = a*b;
-val = a/b;
-val = a%b;
-val = d++
-val = ++d
-val = --d
-val = d--
+// val = a+b;
+// val = a-b;
+// val = a*b;
+// val = a/b;
+// val = a%b;
+// val = d++
+// val = ++d
+// val = --d
+// val = d--
     
-// 2- Atama Oparetörleri
+// // 2- Atama Oparetörleri
 
-val = a;
-val +=a; // val = val + a;
-val -=a; // val = val - a;
-val  *=a; // val = val * a;
-val  /=a; //val = val / a;
-val  %=a; // val =val % a;
-
-
-// 3- Karşılaştırma Oparetörleri
-
-val = a==b;
-val = b==c;
-val = c===b; // değer kontrolü & type
-val= 5=== '5';
-val =  a!=b;
-val = a!==b;
-val = a > b;
-val = b<a;
-val = a >= b; 
-val = 5 >= 5;
-val = a <= b;
-// 4-Mantıksal Oparetörler
-// && (and)
-//true && true =>true
-//true && false =>false
-// false && false => false
-  val= (a>b) && (a>c)
-  val = (a<b) || (a<c)
-  val = !(a>b)
+// val = a;
+// val +=a; // val = val + a;
+// val -=a; // val = val - a;
+// val  *=a; // val = val * a;
+// val  /=a; //val = val / a;
+// val  %=a; // val =val % a;
 
 
-// || (or)
-//true && true =>true
-//true && false =>true
-// false && false => false
-// ! (not)
+// // 3- Karşılaştırma Oparetörleri
 
-//!true =>false
-//!false =>true
+// val = a==b;
+// val = b==c;
+// val = c===b; // değer kontrolü & type
+// val= 5=== '5';
+// val =  a!=b;
+// val = a!==b;
+// val = a > b;
+// val = b<a;
+// val = a >= b; 
+// val = 5 >= 5;
+// val = a <= b;
+// // 4-Mantıksal Oparetörler
+// // && (and)
+// //true && true =>true
+// //true && false =>false
+// // false && false => false
+//   val= (a>b) && (a>c)
+//   val = (a<b) || (a<c)
+//   val = !(a>b)
 
-console.log(val);
-console.log(typeof val);  
+
+// // || (or)
+// //true && true =>true
+// //true && false =>true
+// // false && false => false
+// // ! (not)
+
+// //!true =>false
+// //!false =>true
+
+// console.log(val);
+// console.log(typeof val);  
